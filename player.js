e8aeab853c7071cdc742364b457308006c4a54bd
KodingKamp <kodingkamp@gmail.com>
2019-05-08 12:28:03 -0400

    Moved Files into a Folder

--- player.js
+++
@@ -1,7 +0,0 @@
-// Player variables
-var playerLevel = 1;
-var playerHealth = 100 + (playerLevel - 1) * 3;
-var playerAttackDamage = 40 + (playerLevel - 1) * 3;
-var numHealthPotions = 3;
-var healthPotionHealAmount = 30;
-var healthPotionDropChance = 25; // Percentage
