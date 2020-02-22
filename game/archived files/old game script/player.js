35205837532e3d77c06946c8cc9593ae18317c9f
Kamp Duong <kodingkamp@gmail.com>
2020-02-22 15:20:23 -0500

    updated file structure

--- game/archived files/old game script/player.js
+++
@@ -1,8 +0,0 @@
-// Player variables
-var playerLevel = 1;
-var playerMaxHealth = 100 + (playerLevel - 1) * 3;
-var playerHealth = playerMaxHealth;
-var playerAttackDamage = 40 + (playerLevel - 1) * 3;
-var numHealthPotions = 3;
-var healthPotionHealAmount = 30;
-var healthPotionDropChance = 25; // Percentage
